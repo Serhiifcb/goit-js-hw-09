e4669787955d06eed86fb54ceded9499c2591d6c
Serhii Moroz <sergiy.m.lutsk@gmail.com>
2022-09-27 22:21:14 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const refBody = document.querySelector('body');
+let timerId = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+startBtn.addEventListener('click', () => {
+  startBtn.setAttribute('disabled', 'disabled');
+  timerId = setInterval(() => {
+    colorBody = getRandomHexColor();
+    refBody.style.background = colorBody;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+});
